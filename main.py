55369ef524b6fadb998dd457d53426e31a207bf0
Ermy99 <eizihirwe@gmail.com>
2018-10-29 23:49:03 -0400

    main homepage UI

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, url_for
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def main():
     return render_template("index.html")
+
+@app.route("/institution", methods=['GET'])
+def institution():
+    return render_template("institution.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
